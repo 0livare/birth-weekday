acdb17f87a9e84357c605bfa66841fa2f914de57
Zach Posten <zach@posten.io>
2020-10-09 14:54:58 -0500

    feat: Use math to calcualte sun ray positions

--- src/components/theme-toggle/sun-moon-icon.js
+++ src/components/theme-toggle/sun-moon-icon.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
 export function SunMoonIcon({showSun, ...rest}) {
+  const size = 18
+
   return (
     <svg
-      width='18'
-      height='18'
-      viewBox='0 0 18 18'
+      width='100'
+      height='100'
+      viewBox={`0 0 ${size} ${size}`}
       style={{
         transform: `rotate(${showSun ? 90 : 40}deg)`,
       }}
       {...rest}
     >
       <mask id='moon-mask'>
-        <rect x='0' y='0' width='18' height='18' fill='#FFF'></rect>
+        <rect x='0' y='0' width={size} height={size} fill='white'></rect>
         <circle
           cx={showSun ? 25 : 10}
           cy={showSun ? 0 : 2}
@@ -27,25 +29,39 @@ export function SunMoonIcon({showSun, ...rest}) {
         mask='url(#moon-mask)'
         r={showSun ? 5 : 8}
       ></circle>
-      {/* prettier-ignore */}
       <g>
-        <SunRay isOpen={showSun} cx='17' cy='9' />
-        <SunRay isOpen={showSun} cx='13' cy='15.928203230275509' />
-        <SunRay isOpen={showSun} cx='5.000000000000002' cy='15.92820323027551' />
-        <SunRay isOpen={showSun} cx='1' cy='9.000000000000002' />
-        <SunRay isOpen={showSun} cx='4.9999999999999964' cy='2.071796769724492' />
-        <SunRay isOpen={showSun} cx='13' cy='2.0717967697244912' />
+        {Array(6)
+          .fill(null)
+          .map((_, index) => (
+            <SunRay
+              key={index}
+              isOpen={showSun}
+              size={size}
+              theta={60 * index}
+            />
+          ))}
       </g>
     </svg>
   )
 }
 
-function SunRay({isOpen, cx, cy}) {
+function SunRay({isOpen, theta, size}) {
+  const rayRadius = 1.5
+  const rFromCenter = size / 2 - rayRadius
+
+  const x = rFromCenter * cos(theta)
+  const y = rFromCenter * sin(theta)
+
+  // Convert from cartesian coordinates (4 quadrants)
+  // to SVG coordinates (4th quadrant, 0,0 is top left)
+  const cx = x + size / 2
+  const cy = y + size / 2
+
   return (
     <circle
       cx={cx}
       cy={cy}
-      r='1.5'
+      r={rayRadius}
       fill='currentColor'
       style={{
         transformOrigin: 'center center',
@@ -53,4 +69,16 @@ function SunRay({isOpen, cx, cy}) {
       }}
     ></circle>
   )
+}
+
+function sin(deg) {
+  return Math.sin(degToRad(deg))
+}
+
+function cos(deg) {
+  return Math.cos(degToRad(deg))
+}
+
+function degToRad(deg) {
+  return (deg * Math.PI) / 180
 }
