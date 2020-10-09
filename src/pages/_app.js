56c594512732e3a7d483f5955a0fb52c40815e28
Zach Posten <zach@posten.io>
2020-10-08 21:22:56 -0500

    chore: Convert styles to scss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
+import './global.scss'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
 }
 
